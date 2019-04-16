bd9af05ca5472422587a6598865aaad19d319b76
Sean <sean.kisch@hotmail.com>
2019-04-16 15:10:22 +0200

    Need new branch..

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,90 @@
+<template>
+      <v-app>
+         <v-container>
+            <h1></h1>
+         <v-form mx-auto v-model="valid" ref="form">
+            <v-text-field
+                    placeholder="Enter username"
+                    label="Username"
+                    v-model="username"
+                    :rules="nameRules"
+                    :counter="10"
+                    required
+            ></v-text-field>
+            <v-text-field
+                    placeholder="Enter password"
+                    label="Password"
+                    v-model="password"
+                    :rules="passwordRules"
+                    :counter="10"
+                    required
+                    :type="showPassword ? 'text' : 'password'"
+                    :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                    @click:append="showPassword = !showPassword"
+            ></v-text-field>
+            <v-text-field
+                    placeholder="Enter email"
+                    label="E-mail"
+                    v-model="email"
+                    :rules="emailRules"
+                    required
+            ></v-text-field>
+            <v-checkbox
+                    label="Do you agree?"
+                    v-model="checkbox"
+                    :rules="[(v) => !!v || 'You must agree to continue!']"
+                    required
+            ></v-checkbox>
+
+            <v-btn @click="submit" :class="{ red: !valid, green: valid }">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+         </v-form>
+         </v-container>
+      </v-app>
+</template>
+
+<script>
+   export default {
+      name: "Register",
+      data () {
+         return {
+            valid: false,
+            username: '',
+            password: '',
+            showPassword: false,
+            nameRules: [
+               (v) => !!v || 'Name is required',
+               (v) => v && v.length <= 10 || 'Name must be less than 10 characters'
+            ],
+            passwordRules: [
+               (v) => !!v || 'Password is required',
+               (v) => v && v.length <= 10 || 'Password must be less than 10 characters'
+            ],
+            email: '',
+            emailRules: [
+               (v) => !!v || 'E-mail is required',
+               (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ],
+            select: null,
+            checkbox: true
+         }
+      },
+      methods: {
+         submit () {
+            this.$refs.form.validate()
+         },
+         clear () {
+            this.$refs.form.reset()
+         }
+      }
+   }
+
+
+</script>
+
+<style scoped>
+   h1 {
+      color: black;
+   }
+
+</style>
