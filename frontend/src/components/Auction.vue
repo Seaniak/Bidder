d18d09df74fb457d825b2b74aa7370a379cd1d5e
Peter Sjögren <peter.sjogren@live.se>
2019-04-18 11:52:11 +0200

    Refactor - fuck posts.

--- frontend/src/components/Auction.vue
+++ frontend/src/components/Auction.vue
@@ -1,51 +1,13 @@
 <template>
-  <div id="post" @click="seePostDetails">
-    <div id="post-text">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-    </div>
-    <figure>
-      <img :src="post.imageData" alt="poster">
-    </figure>
-  </div>
+	
 </template>
 
 <script>
-  export default {
-    name: "Post",
-    props: ['post'],
-    methods: {
-      seePostDetails() {
-        this.$router.push({
-          name: 'postDetails',
-          params: {post: this.post}
-        })
-      }
-    }
-  }
+	export default {
+		name: "Auction"
+	}
 </script>
 
 <style scoped>
-  #post {
-    display: flex;
-    text-decoration: none;
-    color: var(--main-font-color);
-    width: 100%;
-    animation-duration: 200ms;
-    border-bottom: solid 1px #7c7e70;
-  }
 
-  #post-text {
-    padding-left: 3%;
-    text-align: start;
-    flex: 4;
-  }
-
-  figure {
-    flex: 2;
-  }
-
-  img {
-    width: 100%;
-  }
 </style>
---
+++ frontend/src/components/AuctionCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="auction" @click="seeAuctionDetails">
+    <div id="auction-text">
+      <h2>{{ auction.title }}</h2>
+      <p>{{ auction.body }}</p>
+    </div>
+    <figure>
+      <img :src="auction.imageData" alt="thumbnail">
+    </figure>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Auction",
+    props: ['auction'],
+    methods: {
+      seeAuctionDetails() {
+        this.$router.push({
+          name: 'auctionDetails',
+          params: {auction: this.auction}
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #auction {
+    display: flex;
+    text-decoration: none;
+    color: var(--main-font-color);
+    width: 100%;
+    animation-duration: 200ms;
+    border-bottom: solid 1px #7c7e70;
+  }
+
+  #auction-text {
+    padding-left: 3%;
+    text-align: start;
+    flex: 4;
+  }
+
+  figure {
+    flex: 2;
+  }
+
+  img {
+    width: 100%;
+  }
+</style>
