5e423ac76732908186f85568c7fbe482a3e1b4e2
Sean Kisch <sean.kisch@hotmail.com>
2019-04-20 04:16:26 +0200

    Vuetify is time consuming..

--- frontend/src/components/AuctionCard.vue
+++ frontend/src/components/AuctionCard.vue
@@ -1,18 +1,52 @@
 <template>
-  <div id="auction" @click="seeAuctionDetails">
+    <v-item-group>
+      <v-container grid-list-md>
+        <v-layout>
+          <v-flex
+                  xs12
+                  md12
+          >
+            <v-item>
+              <v-card @click="seeAuctionDetails"
+                      slot-scope="{ active, toggle }"
+                      class="d-flex"
+                      light
+                      height="200"
+              >
+                <v-img
+                  :src="`https://shopcdn.textalk.se/shop/35510/art10/h3474/132433474-origpic-a14e5f.jpg?max-width=720&max-height=720&quality=85`"
+                >
+                </v-img>
+                <div id="auction-text">
+                <h4>{{ auction.title }}</h4>
+                <h4>Nuvarande bud</h4>
+                <h4>{{ auction.createTime }}</h4>
+                </div>
+              </v-card>
+            </v-item>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-item-group>
+<!--  <div id="auction" @click="seeAuctionDetails">
     <div id="auction-text">
       <h2>{{ auction.title }}</h2>
-      <p>{{ auction.body }}</p>
     </div>
     <figure>
       <img :src="auction.imageData" alt="thumbnail" />
     </figure>
-  </div>
+  </div>-->
+
 </template>
 
 <script>
 export default {
   name: "AuctionCard",
+  data() {
+    return {
+      pictureExists: '' //this.auction.imagePaths
+    }
+  },
   props: ["auction"],
   methods: {
     seeAuctionDetails() {
@@ -26,6 +60,13 @@ export default {
 </script>
 
 <style scoped>
+  .no_bg {
+    background: coral;
+  }
+
+  .bg {
+    background: yellow;
+  }
 #auction {
   display: flex;
   text-decoration: none;
