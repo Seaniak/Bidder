ba81c0d6fda30ed545fc4553fbdf8a3ec6c15585
Peter Sjögren <peter.sjogren@live.se>
2019-04-24 12:38:48 +0200

    AddBidButton

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="bottom-nav">
     <v-btn to="/" icon :active-class="null">
-      <v-icon color="white" large>keyboard_arrow_left </v-icon>
+      <v-icon color="white" large>keyboard_arrow_left</v-icon>
     </v-btn>
 
     <div>
@@ -9,59 +9,66 @@
     </div>
 
     <v-btn
-      icon
-      id="toggleDrawer"
-      @click.stop="$store.state.openNavDrawer = !$store.state.openNavDrawer"
+            icon
+            id="toggleDrawer"
+            @click.stop="$store.state.openNavDrawer = !$store.state.openNavDrawer"
     >
-      <v-icon color="white" medium>menu </v-icon>
+      <v-icon color="white" medium>menu</v-icon>
     </v-btn>
   </nav>
 </template>
 
 <script>
-  import HomeNav from './navbar/HomeNav';
-  import LoginNav from './navbar/LoginNav';
-  import RegisterNav from'./navbar/RegisterNav';
-  import UploadAuction from "./navbar/UploadAuctionNav";
-  import GoToLoginPageNav from "./navbar/GoToLoginPageNav";
+	import HomeNav from './navbar/HomeNav';
+	import LoginNav from './navbar/LoginNav';
+	import RegisterNav from './navbar/RegisterNav';
+	import UploadAuction from "./navbar/UploadAuctionNav";
+	import GoToLoginPageNav from "./navbar/GoToLoginPageNav";
+  import PlaceBid from "./navbar/PlaceBid";
+
 
   export default {
-    name: "Navigation",
-    computed: {
-      currentNavigation() {
-        switch (this.$route.path) {
-          case '/':
-            return HomeNav;
-            break;
-          case '/login':
-            return LoginNav;
-            break;
-          case '/register':
-            return RegisterNav;
-            break;
-          case '/register-success':
-            return GoToLoginPageNav;
-            break;
-          case '/upload':
-            return UploadAuction;
-            break;
-          default:
-            return HomeNav
-        }
-      }
-    }
-  }
+		name: "Navigation",
+		computed: {
+			currentNavigation() {
+				// let path = this.$route.path.substring(0,this.$route.path.lastIndexOf("/"));
+				// console.log(path);
+				switch (this.$route.path.substring(0,this.$route.path.lastIndexOf("/"))) {
+					case '/':
+						return HomeNav;
+						break;
+					case '/login':
+						return LoginNav;
+						break;
+					case '/register':
+						return RegisterNav;
+						break;
+					case '/register-success':
+						return GoToLoginPageNav;
+						break;
+					case '/upload':
+						return UploadAuction;
+						break;
+					case '/auction':
+						return PlaceBid;
+						break;
+					default:
+						return HomeNav
+				}
+			}
+		}
+	}
 </script>
 
 <style scoped>
-#bottom-nav {
-  display: flex;
-  justify-content: space-around;
-  width: 100vw;
-  position: fixed;
-  bottom: 0;
-  height: 10vh;
-  border-top: solid 1px #7c7e70;
-  background-color: teal;
-}
+  #bottom-nav {
+    display: flex;
+    justify-content: space-around;
+    width: 100vw;
+    position: fixed;
+    bottom: 0;
+    height: 10vh;
+    border-top: solid 1px #7c7e70;
+    background-color: teal;
+  }
 </style>
