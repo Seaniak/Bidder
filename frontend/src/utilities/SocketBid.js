3c6a00c9c855598e8275e3adab3ef216507b4241
Peter Sjögren <peter.sjogren@live.se>
2019-05-01 13:44:57 +0200

    fix socketbid

--- frontend/src/utilities/SocketBid.js
+++ frontend/src/utilities/SocketBid.js
@@ -4,26 +4,38 @@ export function notifyBid(state, bid){
   let subscribedAuction = false
 
   let auctionTitle;
-  state.auctions.forEach(a => {
-    if (a.id === bid.auctionId) {
-
-      // check if user has placed a bid on that auction
-      for (let b of a.bids) {
-        if (b.username === state.currentUser.username)
-          console.log(b)
-        subscribedAuction = true
+  if(state.auctions[bid.auctionId]) {
+    state.auctions[bid.auctionId].bids.push(bid);
+    state.auctions[bid.auctionId].maxBid = bid.sum;
+    for (let b of  state.auctions[bid.auctionId].bids) {
+      if (b.username === state.currentUser.username){
+        console.log(b);
+        subscribedAuction = true;
         break;
       }
-      a.bids.push(bid)
-      auctionTitle = a.title
     }
-  })
+  }
+
+  // for(let a of state.auctions){
+  //   if (a.id === bid.auctionId) {
+  //
+  //     // check if user has placed a bid on that auction
+  //     for (let b of a.bids) {
+  //       if (b.username === state.currentUser.username)
+  //         console.log(b)
+  //       subscribedAuction = true
+  //       break;
+  //     }
+  //     a.bids.push(bid)
+  //     auctionTitle = a.title
+  //   }
+  // }
   //  don't notify yourself
   if (bid.username === state.currentUser.username || !subscribedAuction) return;
 
   let notify = {
     icon: 'monetization_on',
-    title: `Utbudad på ${auctionTitle}`,
+    title: `Utbudad på ${state.auctions[bid.auctionId].title}`,
     subtitle: `Nytt bud: ${bid.sum}kr, av ${bid.username}`,
     time: new Date(),
     route: `/auction/${bid.auctionId}`,
